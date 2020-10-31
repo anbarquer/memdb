ea63b78c1360cda6e3c7b6bff2171128ab64ddee
Antonio D. Barquero <anbarquer@gmail.com>
2020-10-31 19:01:11 +0100

    add README.md execution example

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # memdb
 In memory key/value database in pure Python
+
+```
+ $ python3 example.py 
+
+    Filter using Python lambda functions [Person(id=1, name='This is a test', age=23, city='Neverland')]
+    Total Person instances  1
+    Remove last instance added
+    Should be empty []
+    Total Person instances  0
+    Applying complex data filters and measure execution time..
+    Loading 40362 instances
+    Total execution time: 204 ms
+    Applying 43146 filters
+    Total execution time: 1184 ms
+    Can manage arbitrary models
+    Is Garfield over here? [Cat(id=1, age=23, name='Garfield', city='Neverland', favourite_food='Lasagna')]
+    Young cats [Cat(id=2, age=6, name='Mr. Whiskers', city='London', favourite_food='Tuna'), Cat(id=3, age=4, name='Snowball', city='Berlin', favourite_food='Potatos')]
+
+```
--- example.py
+++ example.py
@@ -17,37 +17,40 @@ def populate(entity, fixture) -> None:
 @measure
 def apply_filters(entity, filters) -> None:
     total: int = random.choice(range(MIN, MAX))
-    print(f'FILTER\tApplying {total} filters')
+    print(f'Applying {total} filters')
     for _ in range(total):
         entity.repository.filter_by(random.choice(filters))
 
 
-@measure
-def apply_remove_filters(entity, filters) -> None:
-    total: int = random.choice(range(MIN, MAX))
-    print(f'DELETE\tApplying {total} filters')
-    for _ in range(total):
-        entity.repository.remove_by(random.choice(filters))
-
-
 if __name__ == '__main__':
     Persons = Entity(name='Person', attrs='name age city')
-    Cats = Entity(name='Cat', attrs='age name city favourite_food')
 
-    # Apply complex data filters and measure execution time
+    Persons.repository.add({'name': 'This is a test', 'age': 23, 'city': 'Neverland'})
+    print('Filter using Python lambda functions', Persons.repository.filter_by(lambda x: x.city == 'Neverland'))
+    print(f'Total Person instances  {Persons.repository.total()}')
+    print('Remove last instance added')
+    Persons.repository.pop()
+    print('Should be empty', Persons.repository.filter_by(lambda x: x.city == 'Neverland'))
+    print(f'Total Person instances  {Persons.repository.total()}')
+
+    print('Applying complex data filters and measure execution time..')
     filters = [
         lambda x: x.name.lower().endswith('s'),
         lambda x: x.name.lower().startswith('a'),
         lambda x: x.id in range(MIN, MAX),
-        lambda x: x.id in range(MIN, MAX) or x.city.lower() in ('Madrid', 'Barcelona'),
+        lambda x: x.id in range(MIN, MAX) or x.city.lower() in ('madrid', 'barcelona'),
         lambda x: len(x.city.split()) == 1,
         lambda x: len([i for i in list(x.name)]) == 5 and sum(range(0, x.id)) == 7,
     ]
 
     populate(Persons, get_person_data)
     apply_filters(Persons, filters)
-    apply_remove_filters(Persons, filters)
 
-    populate(Cats, get_cat_data)
-    apply_filters(Cats, filters)
-    apply_remove_filters(Cats, filters)
+    print('Can manage arbitrary models')
+    Cats = Entity(name='Cat', attrs='age name city favourite_food')
+    Cats.repository.add({'name': 'Garfield', 'age': 23, 'city': 'Neverland', 'favourite_food': 'Lasagna'})
+    Cats.repository.add({'name': 'Mr. Whiskers', 'age': 6, 'city': 'London', 'favourite_food': 'Tuna'})
+    Cats.repository.add({'name': 'Snowball', 'age': 4, 'city': 'Berlin', 'favourite_food': 'Potatos'})
+
+    print('Is Garfield over here?', Cats.repository.filter_by(lambda cat: cat.name == 'Garfield'))
+    print('Young cats', Cats.repository.filter_by(lambda cat: cat.age < 10))
